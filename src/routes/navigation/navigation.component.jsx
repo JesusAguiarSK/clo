6b62318c57d58385240c9ba1ea9434669d0c05fa
JesusAguiarSK <j.aguiar@softwarekeep.com>
2023-05-11 11:53:56 -0400

    Cart toggle

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,11 +4,13 @@ import { ReactComponent as CrwLogo} from '../../assets/crown.svg';
 import CartIcon  from '../../components/cart-icon/cart-icon.component';
 import CartDropdown  from '../../components/cart-dropdown/cart-dropdown.component';
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 import './navigation.styles.scss'
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
       <Fragment>
@@ -23,7 +25,7 @@ const Navigation = () => {
             { currentUser ? (<span className='nav-link' onClick={signOutUser}>SIGN OUT</span>) : (<Link className='nav-link' to='/auth'>SIGN IN</Link>) }
             <CartIcon />
           </div>
-          <CartDropdown />
+          {isCartOpen && <CartDropdown />}
         </div>
         <Outlet />
       </Fragment>
