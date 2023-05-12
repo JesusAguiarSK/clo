82a657ee5c34f4e939bd44e04d76883ecd224a4d
JesusAguiarSK <j.aguiar@softwarekeep.com>
2023-05-12 13:10:46 -0400

    Addt to cart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,29 +1,43 @@
 import  { createContext, useState } from 'react';
 
-const addCartItem = (cartItems, productToAdd) => {
-    //find if cartItems contains productToAdd
+export const addCartItem = (cartItems, productToAdd) => {
 
-    //if found, increment quantity
+// Find if cartItems contains productToAdd
 
-    //return new array with modified cartItems/ new cart item
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+//If found, increment quantity
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+// return new array with modified cartItems/ new cart item
+
+return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
 export const CartContext = createContext({
-    isCartOpen: false,
-    setIsCartOpen: () => {},
-    cartItems: [],
-    addItemToCart: () => {},
+  isCartOpen: false,
+  setIsOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
-export const CartProvider = ({children}) => {
-    const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen, setIsCartOpen};
+export const CartProvider = ({ children }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
 
-    const [cartItems, setCartItems] = useState([]);
+  const addItemToCart = (product) =>
+    setCartItems(addCartItem(cartItems, product));
 
-    const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItem(cartItems, productToAdd));
-    };
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
 
-    return <CartContext.Provider value={value}>{children}</CartContext.Provider>
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
