126f604fa4f72f085b0c7c61f6199e2181116c0c
JesusAguiarSK <j.aguiar@softwarekeep.com>
2023-05-17 08:52:49 -0400

    Arrows checkout page

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -45,12 +45,17 @@ export const removeCartItem = (cartItems, cartItemToRemove) => {
 
 };
 
+const clearCartItem = (cartItems, cartItemToClear)  => {
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
   removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
 
 });
@@ -71,7 +76,10 @@ export const CartProvider = ({ children }) => {
   const removeItemFromCart = (cartItemToRemove) =>
     setCartItems(removeCartItem(cartItems, cartItemToRemove));
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount, removeItemFromCart };
+  const clearItemFromCart = (cartItemToClear) =>
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount, removeItemFromCart, clearItemFromCart };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
